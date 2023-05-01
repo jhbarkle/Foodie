e035f0848fcd99995ff60e5bd5d6c2a291270716
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-01 13:57:06 -0400

    fixed issue that causes welcome header title text to disappear on mobile

--- src/components/WelcomeGridSection/WelcomeGridSection.tsx
+++ src/components/WelcomeGridSection/WelcomeGridSection.tsx
@@ -3,9 +3,9 @@ import styles from './WelcomeGridSection.module.scss';
 const WelcomeGridSection = () => {
   return (
     <span className={styles.welcome_container}>
-      <h2 className={styles.welcome_header_title}>
+      <h3 className={styles.welcome_header_title}>
         Discover new recipes with ingredients you have on hand.
-      </h2>
+      </h3>
       <p className={styles.welcome_header_description}>
         Start by adding ingredients to your fridge.
       </p>
