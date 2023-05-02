1877caf3b5daac6bbc6cdbbfc8ca906fc4cd3ca9
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-02 10:53:19 -0400

    added animations to Tip section and added sass variable file

--- src/components/TipGridSection/TipGridSection.tsx
+++ src/components/TipGridSection/TipGridSection.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
 import styles from './TipGridSection.module.scss';
+import { tips } from './Tips';
 
 const TipGridSection = () => {
+  const [tip, setTip] = useState(tips[4]);
+  const [animate, setAnimate] = useState(true);
+
+  useEffect(() => {
+    const timer = setInterval(getRandomTip, 7000);
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  const getRandomTip = () => {
+    const randomNumber = Math.floor(Math.random() * tips.length);
+    setAnimate(true);
+    setTip(tips[randomNumber]);
+    setTimeout(removeAnimationClassName, 3000);
+  };
+
+  // this function is called after 3 seconds to remove
+  // the animation className. This is required to play the
+  // text animation each time a new tip is generated and not
+  // just on pageload.
+  const removeAnimationClassName = () => {
+    setAnimate(false);
+  };
+
   return (
     <span className={styles.section_container}>
       <h3>Food Tip of the Day</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
+      <p className={animate ? styles.animate : styles.no_animate}>{tip}</p>
     </span>
   );
 };
---
+++ src/components/TipGridSection/tips.ts
@@ -0,0 +1,15 @@
+export const tips = [
+  'Pay attention to the interplay of salt, heat, fat, and acid.',
+  'To keep bacon from curling while cooking, lay it flat on a cookie sheet. Edges may touch but not overlap. Bake in the oven at 400 degrees for crispy bacon, about 30 minutes for softer bacon for about 20 minutes. No splatter mess on the stovetop.',
+  'Unbaked cookie dough can be covered and refrigerated for up to 24 hours or frozen in an airtight container for up to nine months.',
+  'Dip cookie cutters in flour or powdered sugar and shake off excess before cutting. For chocolate dough, dip cutters in baking cocoa.',
+  'Instead of folding nuts into the brownie batter, sprinkle them on top of the batter before baking. This keeps the nuts crunchy instead of soggy.',
+  'To test if the spaghetti is done, throw one piece at the wall or ceiling. If it sticks, then it is done.',
+  'Scaling a fish is easier if vinegar is rubbed on the scales first.',
+  'When grilling fish, the rule of thumb is to cook 5 minutes on each side per inch of thickness. Before grilling, rub with oil to seal in moisture.',
+  'A few drops of lemon juice added to simmering rice will keep the grains separated.',
+  'Achieve professionally decorated cakes with a silky, molten look by blow-drying the frosting with a hairdryer until the frosting melts slightly.',
+  'When baking bread, a small dish of water in the oven will help keep the crust from getting too hard or brown.',
+  'To prevent pasta from boiling over, place a wooden spoon or fork across the top of the pot while the pasta is boiling.',
+  'To absorb the pungent odor of foods like fish or cabbage, place a small bowl filled with white vinegar on the stove while cooking.'
+];
