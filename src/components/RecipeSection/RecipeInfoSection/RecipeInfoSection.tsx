a3f4c99a32e47da9bdc40f1fbaa3738868f081e4
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-02 14:26:56 -0400

    added featured recipe title

--- src/components/RecipeSection/RecipeInfoSection/RecipeInfoSection.tsx
+++ src/components/RecipeSection/RecipeInfoSection/RecipeInfoSection.tsx
@@ -9,11 +9,12 @@ interface RecipeInfoSectionProps {
 const RecipeInfoSection: React.FC<RecipeInfoSectionProps> = ({ recipe }) => {
   return (
     <div className={styles.info_container}>
+      <h4 className={styles.featured_recipes_title}>Featured Recipes</h4>
       <div className={styles.info_section}>
         <h2 className={styles.recipe_title}>{recipe.name}</h2>
         <section className={styles.likes_section}>
           <img src="/clock.png" alt="clock" />
-          <p>Cook Time: {recipe.cookTime}</p>
+          <p>Prep Time: {recipe.prepTime} </p>
         </section>
         <section className={styles.recipe_description}>
           {recipe.description}
